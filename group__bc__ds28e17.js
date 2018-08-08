56a37d977d665a8f6591a00738a1eb8c32fbec00
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-08 17:07:49 +0000

    Deploy sdk.bigclown.com to github.com/bigclownlabs/bcf-sdk.git:gh-pages

---
+++ group__bc__ds28e17.js
@@ -0,0 +1,13 @@
+var group__bc__ds28e17 =
+[
+    [ "bc_ds28e17_t", "structbc__ds28e17__t.html", [
+      [ "_channel", "structbc__ds28e17__t.html#a271dcb37bb6992d103bca8e595d30680", null ],
+      [ "_device_number", "structbc__ds28e17__t.html#a01d6233e2314e6271646e804d99026a8", null ]
+    ] ],
+    [ "bc_ds28e17_init", "group__bc__ds28e17.html#ga9111c4df0d96cbb920fb9cf5de06073c", null ],
+    [ "bc_ds28e17_memory_read", "group__bc__ds28e17.html#ga159050bb4986621d5d289433c987b5c0", null ],
+    [ "bc_ds28e17_memory_write", "group__bc__ds28e17.html#gae1baafe53feb279c3556a122782431ec", null ],
+    [ "bc_ds28e17_read", "group__bc__ds28e17.html#gabf29668bdc891947c03db572a40fdb22", null ],
+    [ "bc_ds28e17_set_speed", "group__bc__ds28e17.html#ga5ac9b2a6c35ad5c8fe4ea27f182ef162", null ],
+    [ "bc_ds28e17_write", "group__bc__ds28e17.html#gad24f58c30dd00bc37f941913b49256b7", null ]
+];
